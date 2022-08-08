85dda6cb30b9d4abf7216dd6b199ebac91c7103a
“1147403082@qq.com” <1147403082>
2022-08-08 15:51:12 +0800

    cloud hystrix 监控    http://localhost:端口号/hystrix

--- HyStrix/src/main/java/com/HyStrixMain.java
+++ HyStrix/src/main/java/com/HyStrixMain.java
@@ -6,12 +6,14 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.client.circuitbreaker.EnableCircuitBreaker;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
+import org.springframework.cloud.netflix.hystrix.dashboard.EnableHystrixDashboard;
 
 @SpringBootApplication
 @MapperScan("com/mapper")
 @EnableEurekaClient
 @EnableCircuitBreaker
 @EnableDiscoveryClient
+@EnableHystrixDashboard
 public class HyStrixMain {
     public static void main(String[] args) {
         SpringApplication.run(HyStrixMain.class, args);
